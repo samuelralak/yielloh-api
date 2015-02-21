16d5ad6bbdaea9c398d76a7a63ad60b78e4dca05
samuelralak <samuelralak@ovi.com>
2015-02-21 14:56:44 +0300

    configured oauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::API
 	include ActionController::HttpAuthentication::Basic::ControllerMethods
 	include ActionController::MimeResponds
 	include ActionController::ImplicitRender
-	include ActionController::RequestForgeryProtection
 	include ActionController::Rescue
 	include ActionView::Layouts
+
+	respond_to :html, :json, :xml
 end
