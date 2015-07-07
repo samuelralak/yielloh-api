936f9153df3e183b24f8a48b4456dcf82ca04b41
samuelralak <samuelralak@ovi.com>
2015-07-07 12:34:31 +0300

    added twitter and facebook handles to profile

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,4 +1,4 @@
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :id, :avatar, :fullname, :username, :about, :location, :user
+  attributes :id, :avatar, :fullname, :username, :about, :location, :twitter_handle, :facebook_handle, :user
   # has_one :user, serializer: UserSerializer
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707085202) do
+ActiveRecord::Schema.define(version: 20150707093134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +116,8 @@ ActiveRecord::Schema.define(version: 20150707085202) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "twitter_handle"
+    t.string   "facebook_handle"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
