84fb6bef95403c71371afa117b18a29fac543f07
samuelralak <samuelralak@ovi.com>
2015-07-07 15:18:34 +0300

    changed signout to a post

--- app/controllers/api/v1/user_sessions_controller.rb
+++ app/controllers/api/v1/user_sessions_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::UserSessionsController < ApplicationController
 	before_action :doorkeeper_authorize!
 	skip_before_action :require_profile
 
-	def destroy
+	def logout_user
 		Doorkeeper::AccessToken.revoke_all_for(doorkeeper_token.application_id, current_user)
-		sign_out current_user
-		render json: "OK", status: 200
+		current_user.update_attributes(current_sign_in_at: nil, current_sign_in_ip: nil)
+		render json: { message: "you have successfully logged out"}, status: 200
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ require 'api_constraints'
 require 'subdomain'
 
 Rails.application.routes.draw do
-  use_doorkeeper 
+  use_doorkeeper
   devise_for :users
-  
+
   # API definition
   namespace :api, defaults: { format: :json }, constraints: { subdomain: /.+/ }, path:  '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-    	delete 'user_sessions', to: 'user_sessions#destroy', as: 'logout_user'
+    	post 'user_sessions', to: 'user_sessions#logout_user', as: 'logout_user'
 
       resources :profiles, only: [:show, :create, :update]
       resources :photos, except: [:new, :edit]
